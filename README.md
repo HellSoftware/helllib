4f0124d6b3034df64470309cfd93f57aba78f167
danihek <danihek07@gmail.com>
2024-11-22 00:59:48 +0100

    First broadcast

---
+++ README.md
@@ -0,0 +1,4 @@
+# HElllIB
+
+This repo contains Small, Free, MIT Licensed single header libraries, that you may need:
+- [hell_parser](./hell_parser.h)
---
+++ examples/hell_parser/hell_parser.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define HELL_PARSER_IMPLEMENTATION
+#include "../../hell_parser.h"
+
+int main()
+{
+    char delim = '%';
+    char *input_text = "background=%%color0.hex%%\n \
+                        foreground=%%color15.hex%%";
+
+    /* Initialize parser with input */
+    hell_parser_t *parser = hell_parser_create(input_text);
+    char *buffer = NULL;
+
+    while (!hell_parser_eof(parser))
+    {
+        char ch;
+
+        if (hell_parser_next(parser, &ch) == HELL_PARSER_OK)
+        {
+            if (ch == delim)
+            {
+                parser->pos -= 1; /* We have to do that so hell_parser_delim
+                                     keeps track of the position */
+
+                /* 
+                 * Look out for delim, which is '%',
+                 * count is set to 2, so it will trigger when it finds "%%"
+                 */
+                if (hell_parser_delim(parser, delim, 2, &buffer) == HELL_PARSER_OK)
+                {
+                    printf("Extracted content: '%s'\n", buffer);
+                    free(buffer);
+                }
+            }
+        }
+    }
+
+    hell_parser_destroy(parser);
+    return 0;
+}
